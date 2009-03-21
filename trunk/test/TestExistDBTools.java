9c0f78032c259dca8247cdec558337da3055d37b
alexbleasdale <alexbleasdale@ac71d2fe-724e-0410-bde5-a9d7a306175c>
2009-03-21 16:19:52 +0000

    Adding first unit test

---
+++ trunk/test/TestExistDBTools.java
@@ -0,0 +1,76 @@
+import junit.framework.*; 
+
+import com.netrii.starfruit.xmldb.ExistDBTools;
+
+
+public class TestExistDBTools extends TestCase {
+
+	public void testCreateCollection(){
+		ExistDBTools edbt = new ExistDBTools();
+		
+		try {
+			
+			edbt.startEmbeddedExist();
+			
+			edbt.getCollection("docs2");
+			
+			if (edbt.getCurrentCollection() != "docs2"){
+				// current collection does not exist
+				edbt.createCollection("docs2");
+			}
+			
+			assertEquals("docs2", edbt.getCurrentCollection());
+			int r = edbt.getResourceCount();
+			System.out.println(r);
+			
+			edbt.storeXMLDoc("resources/conf.xml");
+			assertEquals(r+1, edbt.getResourceCount());
+			
+			// second test - switch collections and add docs
+			
+			if(edbt.getCollection("docs") == null){
+				edbt.createCollection("docs");
+			}
+			assertEquals("docs", edbt.getCurrentCollection());
+			
+			int r2 = edbt.getResourceCount();
+			System.out.println(r2);
+			
+			edbt.storeXMLDoc("resources/conf.xml");
+			assertEquals(r2+1, edbt.getResourceCount());
+			
+			
+			if(edbt.getCollection("junit") == null){
+				edbt.createCollection("junit");
+			}
+			assertEquals("junit", edbt.getCurrentCollection());
+			edbt.storeXMLDoc("resources/conf.xml");
+			edbt.getResources();
+			
+			// try creating a child collection from root
+			if(edbt.getCollection("junit/test") == null){
+				edbt.createCollection("junit/test");
+			}
+			assertEquals("junit/test", edbt.getCurrentCollection());
+			edbt.storeXMLDoc("resources/conf.xml");
+			edbt.getResources();
+			
+			if(edbt.getCollection("junit/test/09") == null){
+				edbt.createCollection("junit/test/09");
+			}
+			assertEquals("junit/test/09", edbt.getCurrentCollection());
+			//edbt.returnXMLDocAtIndex(1);
+		    edbt.getXMLDocByName("accdd798.xml");
+			edbt.storeXMLDoc("resources/conf.xml");
+		    edbt.getResources();
+		
+			
+			edbt.shutdownExist();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
