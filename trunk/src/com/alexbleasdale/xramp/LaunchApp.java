cc917d657f05ae81a37afa4231e8e688754309ff
alexbleasdale <alexbleasdale@ac71d2fe-724e-0410-bde5-a9d7a306175c>
2009-07-19 02:02:44 +0000

    updated libs and launch app code

--- trunk/src/com/alexbleasdale/xramp/LaunchApp.java
+++ trunk/src/com/alexbleasdale/xramp/LaunchApp.java
@@ -40,7 +40,7 @@ public class LaunchApp {
         final Map<String, String> initParams = new HashMap<String, String>();
 
         initParams.put("com.sun.jersey.config.property.packages",
-                "com.netrii.starfruit.resources");
+                "com.alexbleasdale.xramp.resources");
 
         System.out.println("Starting grizzly...");
         SelectorThread threadSelector = GrizzlyWebContainerFactory.create(BASE_URI, initParams);
