65723362e126e2dc1b295e8e18be702195e20a7a
alexbleasdale <alexbleasdale@ac71d2fe-724e-0410-bde5-a9d7a306175c>
2009-12-14 01:57:44 +0000

    tidied up the controller for the v0.1 release

--- resources/eXist/treeeditor/controller.js
+++ resources/eXist/treeeditor/controller.js
@@ -5,53 +5,32 @@
 */
 
 // global variables
+var SOURCE_INSTANCE_URI = "/exist/rest/db/treeeditor/flatxml1.xml";
+var DEST_INSTANCE_URI = "/exist/rest/db/treeeditor/flatxml2.xml";
 
 
 /**
 * jQuery TreeEditor Controller v0.1
 */
 $(function () {
-    console.log("DOM ready");
+    //console.log("DOM ready");
     
     // Manage Source Tree
-    
     $("#source-tree").tree({
-      
-      /*
+    
         plugins: {
             contextmenu: {
                 items: {
-                    // get rid of the remove item
                     create: false,
-                    rename: false,
-                    remove: false,
-                    // add an item of our own
-                    my_act: {
-                        label: "Copy to Target",
-                        icon: "", // you can set this to a classname or a path to an icon like ./myimage.gif
-                        visible: function (NODE, TREE_OBJ) {
-                            // this action will be disabled if more than one node is selected
-                            if (NODE.length != 1) return 0;
-                            // this action will not be in the list if condition is met
-                            if (TREE_OBJ.get_text(NODE) == "Child node 1") return - 1;
-                            // otherwise - OK
-                            return 1;
-                        },
-                        action: function (NODE, TREE_OBJ) {
-                            alert(TREE_OBJ.get(NODE, "xml_nested", false));
-                        },
-                        separator_before: true
-                    }
+                    rename: false
                 }
             }
         },
-        
-        */
+     
         data: {
             type: "xml_flat",
             opts: {
-                // TODO - refactor out
-                url: "/exist/rest/db/treeeditor/flatxml1.xml"
+                url: SOURCE_INSTANCE_URI
             }
         },
         
@@ -63,40 +42,26 @@ $(function () {
     });
     
     
-    
     // Manage Target Tree
     $("#target-tree").tree({
         
         plugins: {
             contextmenu: {
                 items: {
-                    // get rid of the remove item
                     create: false,
                     rename: false
                 }
             }
         },
-        
-        
+       
         data: {
             type: "xml_flat",
             opts: {
-                // TODO - refactor out
-                url: "/exist/rest/db/treeeditor/flatxml2.xml"
+                url: DEST_INSTANCE_URI
             }
         },
         
-        
         rules: {
-            clickable: "all",
-            multiple: "on",
-            droppable: "all",
-            deletable: "all",
-            
-            
-            
-            draggable: "all",
-            creatable: "all",
             dragrules: "all",
             multitree: "all"
         }
